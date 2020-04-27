c7010fd3b5a0eeed2eff65f8386ec6a00c930ea8
Gevko <oklahomatmc@gmail.com>
2020-04-27 19:00:29 +0100

    Theme

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="PeeBu"/>
+    <MainPage msg="PeeBu"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainPage from '@/components/MainPage.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
