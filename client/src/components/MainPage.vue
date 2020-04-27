5cbabdb8a0ea42dd7b466481541ed81d8c7eb181
Gevko <oklahomatmc@gmail.com>
2020-04-27 20:40:10 +0100

    Main page - tab panel

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -33,48 +33,10 @@
       <v-toolbar-title>{{title}}</v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+        <v-tabs>
+    <v-tab>Transictions</v-tab>
+    <v-tab>More</v-tab>
+  </v-tabs>
 
     <v-footer app>
       <span>&copy; 2019</span>
