c2cd3d380ac6295db725d26e84e2fc876be44948
Marco <marcooliveirapias@gmail.com>
2020-05-28 13:11:20 +0100

    Filter by source

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,11 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    transactions: []
   },
   mutations: {
+    addTransactions(state, transactions) {
+      state.transactions = transactions;
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    transactionList: state => {
+      return state.transactions
+    }
   }
 })
